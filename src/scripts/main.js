caee8877cc3885b613c964fe042db075bb3d1869
antonyur <ureta.art@hotmail.com>
2020-08-15 00:14:58 -0500

    se soluciono el error del resultado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,22 +5,26 @@ function challenge1(){
     let c = document.getElementById("c-1").value;
 
     operation = `Hola mi nombre es ${a} ${b}  y tengo ${c} años`;
-    console.log(operation);
-    //operation = a + b + c;
 
     document.getElementById("reply1").value = operation;
 }
 
-// function challenge2(){
+function challenge2(){
     
-//     let a = document.getElementById("a-1").value;
-//     let b = document.getElementById("b-1").value;
-//     let c = document.getElementById("c-1").value;
+    let a = document.getElementById("a-2").value;
+    let b = document.getElementById("b-2").value;
+    let c = document.getElementById("c-2").value;
 
+    operation = Math.pow(a, 3)+Math.pow(b, 3)+Math.pow(c, 3);
+
+    document.getElementById("reply2").value = operation;
+}
+
+function challenge3(){
     
+    let a = document.getElementById("a-3").value;
 
-//     document.getElementById("reply1").value = operation;
-// }
+    operation = typeof(a);
 
-//AQUI ESTA EL CSS
-                                                                                                                                                                                                        
+    document.getElementById("reply3").value = operation;
+}
