f1d0468f94983aec7e75bbe2e81dd163589362c9
antonyur <ureta.art@hotmail.com>
2020-08-14 22:57:53 -0500

    falta el js

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+function challenge1(){
+    
+    let a = document.getElementById("a-1").value;
+    let b = document.getElementById("b-1").value;
+    let c = document.getElementById("c-1").value;
+
+    operation = `Hola mi nombre es ${a} ${b}  y tengo ${c} años`;
+    console.log(operation);
+    //operation = a + b + c;
+
+    document.getElementById("reply1").value = operation;
+}
+
+// function challenge2(){
+    
+//     let a = document.getElementById("a-1").value;
+//     let b = document.getElementById("b-1").value;
+//     let c = document.getElementById("c-1").value;
+
+    
+
+//     document.getElementById("reply1").value = operation;
+// }
+
+//AQUI ESTA EL CSS
+                                                                                                                                                                                                        
